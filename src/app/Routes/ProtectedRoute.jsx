5b0324321f28d756a20b5a8ded1885cc284229d8
JahidGittu <jahid.hossen.me@gmail.com>
2025-08-27 06:45:07 +0600

    PrivateRoutes

---
+++ src/app/Routes/ProtectedRoute.jsx
@@ -0,0 +1,28 @@
+// src/app/routes/protectedroute.jsx
+
+"use client";
+
+import { useAuth } from "../Context/AuthContext/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.replace("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading || !user) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
+      </div>
+    );
+  }
+
+  return <>{children}</>;
+}
